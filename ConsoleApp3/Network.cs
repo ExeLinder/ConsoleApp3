2d8c579204c14cee77a952134c30dca0bd55f582
Sergey Ivanov <mailto.sergeyivanov@gmail.com>
2025-05-19 13:08:08 +0300

    added default constr to class Network

--- ConsoleApp3/Network.cs
+++ ConsoleApp3/Network.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Sockets;
 
 namespace ConsoleApp3
 {
     internal class Network
     {
-      
+        TcpListener? listener = null;
+
+        public Network()
+        {
+            listener = new TcpListener(IPAddress.Any, 8000);
+        }
     }
 }
